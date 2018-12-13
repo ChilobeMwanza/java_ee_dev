a09d99970c7be995c8fb1d22f0a91768bfda769e
Chilobe Mwanza <chilobe.mwanza@whistl.co.uk>
2018-12-13 15:15:37 +0000

    few minor changes to helloworld project

---
+++ projects/jsf_tutorials/journal_dev/jsf_hello_world/readme.txt
@@ -0,0 +1,3 @@
+https://www.journaldev.com/6538/jsf-tutorial-for-beginners
+
+a helloworld JSF web app
--- projects/jsf_tutorials/journal_dev/jsf_hello_world/src/com/journaldev/jsf/helloworld/HelloWorld.java
+++ projects/jsf_tutorials/journal_dev/jsf_hello_world/src/com/journaldev/jsf/helloworld/HelloWorld.java
@@ -8,18 +8,20 @@ import javax.faces.bean.SessionScoped;
 
 @ManagedBean(name="helloWorld")
 @SessionScoped
-public class HelloWorld implements Serializable{
+public class HelloWorld implements Serializable
+{
     
     private static final long serialVersionUID = -6913972022251814607L;
 	
     private String s1 = "Hello World!!";
 
-    public String getS1() {
-        System.out.println(s1);
+    public String getS1() 
+    {
         return s1;
     }
 
-    public void setS1(String s1) {
+    public void setS1(String s1) 
+    {
         this.s1 = s1;
     }
   
